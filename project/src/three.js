332dbaf612a1870add467fbe2c9b23b3d88830da
lkm43721 <150585159+lkm43721@users.noreply.github.com>
2025-04-05 01:26:22 +0900

    testcase1

--- project/src/three.js
+++ project/src/three.js
@@ -1,4 +1,4 @@
-import * as THREE from "../node_modules/three/build/three.module.js";
+import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
 
 // 타원체 지구 크기 설정
 const radiusX = 1;
